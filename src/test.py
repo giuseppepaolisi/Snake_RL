957688f2c7c2b7141cb3243561058ed8aa20361e
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-11-27 21:14:35 +0100

    Update main

--- src/test.py
+++ src/test.py
@@ -1,32 +1,20 @@
 
-from env.snake.grid import Grid
 from env.snake_env import Snake_Env
+
 done = False
-i = 0
-#while not done:
-"""action = env.action_space.sample()  # Random action
-obs, reward, done, info = env.step(action)
-env.render()
-print(f"{i} Reward: {reward}, Score: {env.score}")
-i+=1"""
-#game = GameEnvironment(grid_size=(10, 10), cell_size=30)
-#game.run()
-
-#game = Game(grid_size=(10, 10), cell_size=30)
-#game.render()
-"""snake = Snake()
-action = snake.action_space.sample()
-snake.step(action)
-snake.render()"""
-#env.close()
-
 env = Snake_Env()
 i = 0
-while i<10000:
-    action = env.action_space.sample()
-    grid, reward, done, dict =env.step(action)
-    env.render()
-    print(f"{i} Reward: {reward}, Score: {dict['score']}")
+
+while i<100:
+    ricompensa_cum = 0
+    while not done:
+        action = env.action_space.sample()
+        grid, reward, done, dict =env.step(action)
+        env.render()
+        print(f"{i} Reward: {reward}, Score: {dict['score']}")
+        ricompensa_cum += reward
+    done = False
+    env.reset()
+    print(f"\n\t***episodio: {i} ricompensa {ricompensa_cum}\n")
     i+=1
-    if(done):
-        env.reset()
+env.close()
