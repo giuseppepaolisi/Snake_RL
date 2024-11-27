634194869f9f432bca650f0fe8b3cf1feb37f0d7
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-11-27 21:47:58 +0100

    Add register of env

--- src/test.py
+++ src/test.py
@@ -1,12 +1,21 @@
 
-from env.snake_env import Snake_Env
+import gym
+from gym.envs.registration import register
 
+# Registrazione dell'ambiente Snake
+register(
+    id='Snake-v0',
+    entry_point='__main__:Snake_Env',
+)
+
+# Creazione dell'ambiente
+env = gym.make('Snake-v0')
 done = False
-env = Snake_Env()
 i = 0
 
 while i<100:
     ricompensa_cum = 0
+    env.reset()
     while not done:
         action = env.action_space.sample()
         grid, reward, done, dict =env.step(action)
@@ -14,7 +23,6 @@ while i<100:
         print(f"{i} Reward: {reward}, Score: {dict['score']}")
         ricompensa_cum += reward
     done = False
-    env.reset()
     print(f"\n\t***episodio: {i} ricompensa {ricompensa_cum}\n")
     i+=1
 env.close()
