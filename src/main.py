6b7d82b43a9daa7477a4740238290c23f4cd1f4b
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2024-12-12 12:45:43 +0100

    Update main

--- src/main.py
+++ src/main.py
@@ -13,34 +13,31 @@ def main():
         entry_point='env.snake_env:Snake_Env',
         max_episode_steps=300,
     )
-    env = Snake_Env(size=5)
+    size = 5
+    env = Snake_Env(size=size)
 
     # Calcola grandezza stato per DQN
     state_size = env.size * 2 + 2  # Lunghezza massima del serpente * 2 (coordinate x,y) + posizione della mela (2)
-    print(type(env.action_space))
-    print(env.action_space)
-    print("Action space type:", type(env.action_space))
-    print("Action space details:", env.action_space)
     action_size = env.action_space.n
     episodes=10000
     max_steps=200
     
     # Create and train DQN agent
-    dqn_agent = DQNAgent(state_size, action_size, learning_rate=0.01, gamma=0.95, epsilon=0.9, episodes=episodes)
-    dqn_train = Train(env, dqn_agent, episodes, max_steps=max_steps)
-    dqn_train.train()
+    #dqn_agent = DQNAgent(state_size, action_size, learning_rate=0.01, gamma=0.95, epsilon=0.9, episodes=episodes)
+    #dqn_train = Train(env, dqn_agent, episodes, max_steps=max_steps)
+    #dqn_train.train()
 
     """sarsa_agent = Sarsa(state_size, action_size, learning_rate=0.01, gamma=0.95, epsilon=0.9, episodes=episodes)
     #agent.load('models/snake_q_agent.pkl')
     sarsa_train = Train(env, sarsa_agent, episodes, max_steps=max_steps)
-    sarsa_train.train()
+    sarsa_train.train()"""
     
     
     q_learning_agent = QLearningAgent(state_size, action_size, learning_rate=0.01, gamma=0.95, epsilon=0.9, episodes=episodes)
-    q_learning_agent_train = Train(env, q_learning_agent, episodes, max_steps=max_steps)
-    q_learning_agent_train.train()"""
+    #q_learning_agent_train = Train(env, q_learning_agent, episodes, max_steps=max_steps)
+    #q_learning_agent_train.train()
 
-    #agent.load(f'models/snake_{agent.get_model()}_{episodes}.pkl')
+    q_learning_agent.load(f'models/snake_{q_learning_agent.get_model()}_{episodes}.pkl')
     
     # Dopo l'addestramento di entrambi gli agenti, genera grafico combinato per lo score
     """plot_combined_metric(
@@ -51,7 +48,7 @@ def main():
     
     #Esecuzione di entrambi gli agent per testare le loro performance
     #for agent, agent_name in zip ([sarsa_agent, q_learning_agent], ['Sarsa', 'Q-Learning']):
-    env = Snake_Env(size=5, render_mode='human')  # Instantiate environment directly again
+    env = Snake_Env(size=size, render_mode='human') 
     num_episodes = 10
     for episode in range(num_episodes):
         state, info = env.reset()
@@ -61,7 +58,7 @@ def main():
         print(f"--- Episode {episode + 1} ---")
 
         while True:
-            action = dqn_agent.choose_action(state)  # Changed to dqn_agent
+            action = q_learning_agent.choose_action(state)
             next_state, reward, done, truncated, info = env.step(action)
             total_reward += reward
             state = next_state
