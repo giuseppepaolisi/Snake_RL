02e91b399ffad8a6646d4ffcc0d8b9aee4950acb
giuseppepaolisi <giuseppe.paolisi@gmail.com>
2025-01-31 14:46:55 +0100

    Balance rewards

--- src/env/snake/const/rewards.py
+++ src/env/snake/const/rewards.py
@@ -1,6 +1,6 @@
 # Ricompense
-REWARD_FOOD = 50 # Ricompensa per mangiare il cibo
+REWARD_FOOD = 5 # Ricompensa per mangiare il cibo
 REWARD_STEP_TO_FOOD = 1 # Ricompensa per ogni passo effettuato nella direzione del cibo
 REWARD_STEP_NO_FOOD = -1 # Penalità per ogni passo effettuato senza andare nella direzione del cibo
-REWARD_COLLISION = -100 # Penalità per una collisione
-REWARD_COLLISION_SELF = -100 # Penalità per una collisione con se stesso
+REWARD_COLLISION = -10 # Penalità per una collisione
+REWARD_COLLISION_SELF = -10 # Penalità per una collisione con se stesso
